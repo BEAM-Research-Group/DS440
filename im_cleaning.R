9f030a6030adf4b50d462ccdb3f78bd712f65c27
mxh565 <42879498+mxh565@users.noreply.github.com>
2021-03-09 09:17:08 -0500

    im_cleaning
    Reads images, randomly puts them into training, testing, and validation sets based on porportion, and converts images to .jpg
    
    Debugging function read_convert_save that saves the images in folder train, test, or validation respectfully for later use in modeling.

---
+++ im_cleaning.R
@@ -0,0 +1,89 @@
+# Front Matter
+rm(list=ls())
+gc()
+# set seed for reproducibility 
+set.seed(440)
+
+# NOTES:
+  # Should pre-damaged building image be included as input?
+  # Yes, could use them to help predict no damage -> just make sure to label them as no damage
+
+# Import libraries
+library(magick)
+library(EBImage)
+library("readxl")
+
+# save directory 
+im_dir = "../data/PRJ-2301/D1. Building Assessments/Photographs/" 
+# there are folders names 1 through 233
+folder_names = 1:233
+
+# get images
+# go through each folder
+im_names = c()
+for (folder in folder_names)
+{ 
+  # get image file names in current folder
+  cur_path = paste0(im_dir, folder)  # current path
+  im_PNG = list.files(path = cur_path, pattern = '*.PNG')
+  im_jpg = list.files(path = cur_path, pattern = '*.jpg')
+  im_JPG = list.files(path = cur_path, pattern = '*.JPG')
+  # combine into main list of names
+  im_names = c(im_names, im_PNG, im_jpg, im_JPG)
+}   # end for (folder in folder_names)
+
+# split into train, validate, and test sets
+# get count of all images
+len_im = length(im_names)
+# portion of data that's for testing
+test_per = 0.2
+# take ceiling since can't have fractional count of data in a set
+len_test = ceiling(test_per * len_im)  
+# portion of data that's for training
+train_per = 0.6  
+len_train = ceiling(train_per * len_im)
+# that leaves 20% for validation
+# sample
+# permute image names randomly
+im_names_shuffled = sample(im_names)  # does not overwrite im_names
+# the first randomly placed names are used in testing
+test_im = im_names_shuffled[1:len_test]
+# the next set randomly placed names are used in training
+train_im = im_names_shuffled[(len_test + 1):(len_test + len_train)]
+# the rest of the data are used in validation 
+val_im = im_names_shuffled[(len_test + len_train + 1):len_im]
+
+read_convert_save('test', test_im)
+read_convert_save('train', train_im)
+read_convert_save('val', val_im)
+
+# FUNCTIONS 
+read_convert_save = function(type, im_list)
+  {
+  # INPUT: 
+    # type: type of set i.e. 'train', 'test', 'val'
+    # im_list: list of the image names
+  # OUTPUT: 
+    # [optional] images: list of images (converted to .jpg if needed)
+  # NOTE: 
+    # Could potentially add transformations here, but
+    # I'm pretty sure there's a parameter
+  images = vector(mode = "list", length = length(im_names))
+  if (length(im_list) > 0)  # checks if there are images
+  {
+    for (im_ in im_list)  # go through each image
+    {
+      # get image file
+      im_file = system.file(paste0(im_dir, type, '/', im_), package="EBImage") 
+      # read image into r environment 
+      im = readImage(im_file) 
+      # convert to jpg and save
+      # *Bug*
+      writeImage(x = im, 
+                 files = im_names, 
+                 path = paste0('../data/', type, '/', im), 
+                 type = 'jpg')
+    }  # end for
+  } # end if
+  else {print('No images provided for input.')}
+  } # end function
