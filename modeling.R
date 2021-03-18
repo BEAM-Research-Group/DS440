11d9235258101cc381a9c5dc8aa7842c7565d655
mxh565 <42879498+mxh565@users.noreply.github.com>
2021-03-17 22:28:30 -0400

    Update modeling.R
    Added image transformations and cleaned up formatting

--- modeling.R
+++ modeling.R
@@ -1,36 +1,50 @@
 library(keras)
+# library(tensorflow)
+
+# image_data_generator transformations 
+# https://towardsdatascience.com/exploring-image-data-augmentation-with-keras-and-tensorflow-a8162d89b844
+# https://keras.rstudio.com/reference/image_data_generator.html
+
+# Basic example of image cnn in R
+# https://www.shirin-glander.de/2018/06/keras_fruits/
 
 ##################### MODELING #####################
 batch_size = 32
-train_data_gen= image_data_generator(rescale = 1/255,
-                                     validation_split = 0.3)
-train_image_array_gen  = flow_images_from_directory("./COVID-19 Radiography Database/",
-                                                    train_data_gen,
-                                                    subset = 'training',
+data_gen= image_data_generator(rotation_range     = 90,
+                               width_shift_range  = 0.5,
+                               height_shift_range = 0.5,
+                               # brightness_range = c(0.2, 1),
+                               shear_range        = 40,
+                               zoom_range         = 40,
+                               horizontal_flip    = TRUE,
+                               vertical_flip      = TRUE,
+                               rescale            = 1/255)
+
+train_image_array_gen  = flow_images_from_directory(directory   = '../data/clean_im/train',
+                                                    generator   = train_data_gen,
+                                                    subset      = 'training',
                                                     target_size = c(20, 20),
-                                                    class_mode = "categorical",
-                                                    batch_size = batch_size,
-                                                    seed = 42,
-                                                    color_mode = 'grayscale')
+                                                    class_mode  = "categorical",
+                                                    batch_size  = batch_size,
+                                                    seed        = 42,
+                                                    color_mode  = 'grayscale')
 
-valid_image_array_gen  = flow_images_from_directory("./COVID-19 Radiography Database/",
-                                                    train_data_gen,
-                                                    subset = 'validation',
+valid_image_array_gen  = flow_images_from_directory(directory   = '../data/clean_im/val',
+                                                    generator   = data_gen,
+                                                    subset      = 'validation',
                                                     target_size = c(20, 20),
-                                                    class_mode = "categorical",
-                                                    batch_size = batch_size,
-                                                    seed = 42,
-                                                    color_mode = 'grayscale')
+                                                    class_mode  = "categorical",
+                                                    batch_size  = batch_size,
+                                                    seed        = 42,
+                                                    color_mode  = 'grayscale')
 
-test_data_gen = image_data_generator(rescale = 1/255)
-
-test_image_array_gen = flow_images_from_directory("./COVID-19 Radiography Database/", 
-                                                  test_data_gen,
-                                                  target_size = c(20, 20),
-                                                  class_mode = "categorical",
-                                                  batch_size = batch_size,
-                                                  seed = 42,
-                                                  color_mode = 'grayscale')
+test_image_array_gen = flow_images_from_directory(directory     = '../data/clean_im/test', 
+                                                  generator     = data_gen,
+                                                  target_size   = c(20, 20),
+                                                  class_mode    = "categorical",
+                                                  batch_size    = batch_size,
+                                                  seed          = 42,
+                                                  color_mode    = 'grayscale')
 
 cat("number of images per class:")                         
 table(factor(train_image_array_gen$classes))
@@ -57,17 +71,15 @@ model%>%
   layer_dense(units = 3, activation = 'softmax')
 
 model %>%
-  layer_conv_2d(filter = 32, 
+  layer_conv_2d(filter      = 32, 
                 kernel_size = c(3,3), 
-                padding = "same", 
+                padding     = "same", 
                 input_shape = c(20, 20, 3)) %>%
-  layer_activation("relu") #%>%
+  layer_activation("relu") 
 
 # Second hidden layer
-layer_conv_2d(filter = 16, kernel_size = c(3,3), padding = "same") %>%
+layer_conv_2d(filter      = 16, 
+              kernel_size = c(3,3), 
+              padding     = "same") %>%
   layer_activation_leaky_relu(0.5) %>%
-  layer_batch_normalization() #%>%
-
-
-
-
+  layer_batch_normalization() 
