7439f4f3a2d79adb73ad6df1c2599d21fafd325b
mxh565 <42879498+mxh565@users.noreply.github.com>
2021-04-05 15:08:26 -0400

    Update modeling.R
    Debugged and included compiling the model, fitting the model, and preliminary visualization code

--- modeling.R
+++ modeling.R
@@ -1,15 +1,36 @@
+# clean environment
+rm(list=ls()) # removes environment variables 
+gc() # garbage collection
+
+# install as needed
+#install.packages("BiocManager") 
+#BiocManager::install("EBImage")
+
+# libraries 
+library(reticulate)
 library(keras)
-# library(tensorflow)
+library(tensorflow)
+library(tidyverse)
+#library(kerasR) # ABORTS SESSION IF RUN
+#library(imager)
+
+# set up reticulate anaconda environment 
+# since keras depends on it for python packages 
+# specifically, fit_generator() from keras requires PIL 
+# from python pillow library 
+conda_list()[[1]][1] %>% 
+  use_condaenv(required = TRUE)
 
 # image_data_generator transformations 
 # https://towardsdatascience.com/exploring-image-data-augmentation-with-keras-and-tensorflow-a8162d89b844
 # https://keras.rstudio.com/reference/image_data_generator.html
 
-# Basic example of image cnn in R
+# Basic example of image cnn in R 
 # https://www.shirin-glander.de/2018/06/keras_fruits/
 
-##################### MODELING #####################
+##################### MODELING #################################################
 batch_size = 32
+
 data_gen= image_data_generator(rotation_range     = 90,
                                width_shift_range  = 0.5,
                                height_shift_range = 0.5,
@@ -20,8 +41,11 @@ data_gen= image_data_generator(rotation_range     = 90,
                                vertical_flip      = TRUE,
                                rescale            = 1/255)
 
-train_image_array_gen  = flow_images_from_directory(directory   = '../data/clean_im/train',
-                                                    generator   = train_data_gen,
+# make sure sub directories are set up 
+# (directory points to folder that contains a folder that has images)
+
+train_image_array_gen  = flow_images_from_directory(directory   = 'train',
+                                                    generator   = data_gen,
                                                     subset      = 'training',
                                                     target_size = c(20, 20),
                                                     class_mode  = "categorical",
@@ -29,7 +53,7 @@ train_image_array_gen  = flow_images_from_directory(directory   = '../data/clean
                                                     seed        = 42,
                                                     color_mode  = 'grayscale')
 
-valid_image_array_gen  = flow_images_from_directory(directory   = '../data/clean_im/val',
+valid_image_array_gen  = flow_images_from_directory(directory   = 'val',
                                                     generator   = data_gen,
                                                     subset      = 'validation',
                                                     target_size = c(20, 20),
@@ -38,7 +62,7 @@ valid_image_array_gen  = flow_images_from_directory(directory   = '../data/clean
                                                     seed        = 42,
                                                     color_mode  = 'grayscale')
 
-test_image_array_gen = flow_images_from_directory(directory     = '../data/clean_im/test', 
+test_image_array_gen = flow_images_from_directory(directory     = 'test', 
                                                   generator     = data_gen,
                                                   target_size   = c(20, 20),
                                                   class_mode    = "categorical",
@@ -68,18 +92,51 @@ model = keras_model_sequential()
 model%>%
   layer_dense(units = 256, activation = 'relu', input_shape = c(128*128*3)) %>%
   layer_dense(units = 128, activation = 'relu') %>%
-  layer_dense(units = 3, activation = 'softmax')
-
-model %>%
+  layer_dense(units = 3, activation = 'softmax') %>%
   layer_conv_2d(filter      = 32, 
                 kernel_size = c(3,3), 
                 padding     = "same", 
                 input_shape = c(20, 20, 3)) %>%
-  layer_activation("relu") 
-
-# Second hidden layer
-layer_conv_2d(filter      = 16, 
-              kernel_size = c(3,3), 
-              padding     = "same") %>%
-  layer_activation_leaky_relu(0.5) %>%
+  layer_activation("relu") %>%
+  # second hidden layer
+  layer_conv_2d(filter      = 16, 
+                kernel_size = c(3,3), 
+                padding     = "same") %>%
+  layer_activation_leaky_relu(0.5)  %>%. # layer_activation("relu") %>%
   layer_batch_normalization() 
+
+# check model structure (layer summary)
+summary(model)
+
+# compile
+model %>% compile(
+  loss = "categorical_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 0.0001, decay = 1e-6),
+  metrics = "accuracy"
+)
+
+# fit
+hist = model %>% fit_generator(
+  # training data
+  train_image_array_gen,
+  # epochs
+  steps_per_epoch = as.integer(train_samples / batch_size), 
+  epochs = epochs, 
+  # validation data
+  validation_data = valid_image_array_gen,
+  validation_steps = as.integer(valid_samples / batch_size),
+  # print progress
+  verbose = 2,
+  callbacks = list(
+    # save best model after every epoch
+    callback_model_checkpoint(save_best_only = TRUE),
+    
+    # only needed for visualizing with TensorBoard
+    callback_tensorboard("/logs")
+  )
+)
+
+# visualize the history 
+plot(hist)
+
+tensorboard(log_dir = "/logs")
